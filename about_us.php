817e67b23b24aa0aceab48dac39d527c695d5710
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2017-01-15 19:21:38 +0000

    more content

--- about_us.php
+++ about_us.php
@@ -16,9 +16,112 @@ print_header("About Aberdeen PHP", "What's is all about at AberdeenPHP");
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                <p>History and background.</p>
+                <h1>History and background.</h1>
+                <p>Content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content.</p>
+                <p>Content content content.</p>
+                <p>Content content content content content content content content content content content content content content content.</p>
+                <p>Content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content content.</p>
+                <p></p>
+                <hr/>
             </div>
         </div>
+
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <h2>Some various photos of AberdeenPHP</h2>
+            </div>
+        </div>
+                
+        <div class="row">
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_rasmus_2016_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p>             
+            </div>
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_august_2015_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p> 
+            </div>            
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_polybag_raffle_october_2015_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p>             
+            </div>          
+        </div>        
+        
+        <div class="row">
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_febuary_2015_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p>             
+            </div>
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_june_2016_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p> 
+            </div>            
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_november_2016_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p> 
+            </div>            
+        </div>  
+        
+        <div class="row">
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_continousphp_september_2015_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p> 
+            </div>          
+        
+        
+
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_september_2015_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p> 
+            </div>            
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_birthday_cake_april_2016_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p> 
+            </div>            
+        </div>  
+        
+        <div class="row">
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_september_2015_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p>             
+            </div>
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_stickers_october_2015_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p> 
+            </div>            
+            <div class="col-md-4">
+                <p>
+                    <img src="images/aberdeenphp_meet_thomas_cat_october_2015_small.jpg" alt="tbc" class="img-rounded"><br/>
+                    tbc
+                </p> 
+            </div>            
+        </div>                  
+
+
+        
+        
     </div>
 
     <hr>
