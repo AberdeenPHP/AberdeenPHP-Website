41d2b98badbc36eb9a6140caa5c70c91d3f29b16
Rob Cooper <sunscreem@gmail.com>
2017-03-04 13:39:11 +0000

    Events page fixed - events with no venue won't crash the page now. Part of #12

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -15,20 +15,22 @@
                     <p> {!! nl2br( $event->start->displaytimezone ) !!} </p>
                     <p> {!! nl2br( $event->description ) !!} </p>        
                     
-                    <p>
-                        @if ( $event->venue->title ) {!! $event->venue->title . '<br/>' !!} @endif
-                        @if ( $event->venue->description ) {!! $event->venue->description . '<br/>' !!} @endif
-                        @if ( $event->venue->address ) {!! $event->venue->address . '<br/>' !!} @endif
-                        @if ( $event->venue->addresscode ) {!! $event->venue->addresscode . '<br/>' !!} @endif
+                    @if(isset($event->venue))
+                        <p>
+                            @if ( $event->venue->title ) {!! $event->venue->title . '<br/>' !!} @endif
+                            @if ( $event->venue->description ) {!! $event->venue->description . '<br/>' !!} @endif
+                            @if ( $event->venue->address ) {!! $event->venue->address . '<br/>' !!} @endif
+                            @if ( $event->venue->addresscode ) {!! $event->venue->addresscode . '<br/>' !!} @endif
 
-                        @if ( isset( $event->venue->lat ) AND isset( $event->venue->lng ) ) 
-                            <a href="https://www.google.com/maps/{!! '@'.$event->venue->lat .','. $event->venue->lng . ',17z' !!}" 
-                               target="_blank">
-                                View on location Google Maps
-                            </a>
-                            <br/>
-                        @endif
-                    </p>
+                            @if ( isset( $event->venue->lat ) AND isset( $event->venue->lng ) ) 
+                                <a href="https://www.google.com/maps/{!! '@'.$event->venue->lat .','. $event->venue->lng . ',17z' !!}" 
+                                   target="_blank">
+                                    View on location Google Maps
+                                </a>
+                                <br/>
+                            @endif
+                        </p>
+                    @endif
                     <p>
                         <a href="{{ $event->url }}" target="_blank">View event on OpenTechCalendar</a> 
                     </p>
