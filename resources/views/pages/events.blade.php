fbe1049a1f39f5a0c0123b6fb2c6c6168d65b2ae
Danny Wilson <danny@iamdannywilson.com>
2017-03-11 08:44:01 +0000

    removing extra file

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -11,7 +11,31 @@
 
                 @forelse ($category['events']->data as $event)
 
-                    @include('components.event', array('event' => $event))
+                    <h3>{{ $event->summary }}</h3>
+                    <p> {!! nl2br( $event->start->displaytimezone ) !!} </p>
+                    <p> {!! nl2br( $event->description ) !!} </p>
+
+                    @if(isset($event->venue))
+                        <p>
+                            @if ( $event->venue->title ) {!! $event->venue->title . '<br/>' !!} @endif
+                            @if ( $event->venue->description ) {!! $event->venue->description . '<br/>' !!} @endif
+                            @if ( $event->venue->address ) {!! $event->venue->address . '<br/>' !!} @endif
+                            @if ( $event->venue->addresscode ) {!! $event->venue->addresscode . '<br/>' !!} @endif
+
+                            @if ( isset( $event->venue->lat ) AND isset( $event->venue->lng ) )
+                                <a href="https://www.google.com/maps/{!! '@'.$event->venue->lat .','. $event->venue->lng . ',17z' !!}"
+                                   target="_blank">
+                                    View on location Google Maps
+                                </a>
+                                <br/>
+                            @endif
+                        </p>
+                    @endif
+                    <p>
+                        <a href="{{ $event->url }}" target="_blank">View event on OpenTechCalendar</a>
+                    </p>
+                    <p><br/><br/></p>
+                    <hr/>
 
                 @empty
                     <p style="padding: 30px 0"><em >No events scheduled.</em></p>
