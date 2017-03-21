19c1389e13ebdd777c014ccf8fe43b28c378b5eb
Rob Cooper <sunscreem@gmail.com>
2017-03-21 18:34:24 +0000

    BUg fix: The events/aberdeen page wasnt loading an image. It is now.

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -19,7 +19,7 @@ class ViewServiceProvider extends ServiceProvider
             $dir = "./images/random_header_images/";
             $images = glob($dir . '*.{jpg,jpeg,png}', GLOB_BRACE);
             shuffle($images);
-            $randomBackgroundImage = array_pop($images);
+            $randomBackgroundImage = subtr(array_pop($images),1);
 
             $view->with('randomBackgroundImage', $randomBackgroundImage);
         });
