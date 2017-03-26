e8a12f1beacad2ee587ac40614f055c000b78c3c
Rob Cooper <sunscreem@gmail.com>
2017-03-26 13:35:16 +0100

    Fixed the homepage image to always be us in the pub

--- app/Http/Controllers/GeneralPagesController.php
+++ app/Http/Controllers/GeneralPagesController.php
@@ -10,7 +10,8 @@ class GeneralPagesController extends Controller
 	public function home(){
 
 		return view('pages.homepage',['title'=>"Aberdeen PHP",
-									  'tagline'=>"The heart of Aberdeen's Web development community"]); 
+									  'tagline'=>"The heart of Aberdeen's Web development community",
+									  'image'=>'/images/random_header_images/aberdeen_08.jpg']); 
 	}
 
 	public function about() { 
