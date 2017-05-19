36c72b8b420b7d7067107c07635944f48dd59379
Rob Cooper <sunscreem@gmail.com>
2017-05-19 18:40:46 +0100

    Home page changes - part of #25

--- resources/views/pages/homepage.blade.php
+++ resources/views/pages/homepage.blade.php
@@ -9,11 +9,11 @@
             together and discuss just about anything in and around the PHP Community.</p>
             <p>We usually meet on the first Wednesday of each month and run talks or open conversations around a variety of tech topics; with a 
             focus on PHP development but extending to general development practises.</p>
-            <p>We usually meet at Fifth Ring, who kindly let us use a room. They're at 47-49 Huntly Street, Aberdeen, AB10 1TH.  If 
-            the door is shut, just knock on the window to right of the door.  We usually start at 6:30 and run till about 8:00 or 8:30.</p>
-            <p>We usually have pizza, beer and soft drinks.</p>	
-            <p>The group is a diverse, welcoming, sociable, relaxed, irreverent, informative and fun.</p>	
-            <p>It is probably a good idea to check AberdeenPHP on Twitter a couple of days before the meeting, incase the time or venue has changed.</p>	
+            <p>We usually meet at Fifth Ring, who kindly let us use a room. They're at <a href="https://www.google.co.uk/maps/place/Fifth+Ring+Ltd/@57.145627,-2.1102727,17z/data=!3m1!4b1!4m5!3m4!1s0x48840e2407f91149:0xa38e0f81685d16e4!8m2!3d57.145627!4d-2.108084">47-49 Huntly Street, Aberdeen, AB10 1TH</a>.  If 
+            the door is shut, just knock on the window to right of the door.  We usually start at <time>6:30pm</time> and run till about around 8:30 or 9:00pm.</p>
+            <p>We provide pizza, beer, soft drinks and terrible jokes.</p>	
+            <p>The group is diverse, welcoming, sociable, relaxed, irreverent, informative and fun.</p>	
+            <p>It is probably a good idea to check <a href="https://twitter.com/aberdeenphp">AberdeenPHP on Twitter</a> a couple of days before the meeting, incase the time or venue has changed.</p>	
         </div>
         <div class="col-md-4">
             <a 
