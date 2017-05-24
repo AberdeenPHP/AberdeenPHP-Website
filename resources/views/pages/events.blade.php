81b49e6879d3c41a8658a2e772e8376fe98f6beb
Rob Cooper <sunscreem@gmail.com>
2017-05-24 18:55:32 +0100

    added venue name search to the google maps links. part of #25

--- resources/views/pages/events.blade.php
+++ resources/views/pages/events.blade.php
@@ -6,15 +6,14 @@
     <div class="row">
         <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
             @foreach($events as $category)
-           
+            
                 <h2>{{ $category['sectionname'] }}</h2>
 
                 @forelse ($category['events']->data as $event)
-
+        
                     <h3>{{ $event->summary }}</h3>
                     <p> {!! nl2br( $event->start->displaytimezone ) !!} </p>
                     <p> {!! nl2br( $event->description ) !!} </p>
-
                     @if(isset($event->venue))
                         <p>
                             @if ( $event->venue->title ) {!! $event->venue->title . '<br/>' !!} @endif
@@ -23,7 +22,7 @@
                             @if ( $event->venue->addresscode ) {!! $event->venue->addresscode . '<br/>' !!} @endif
 
                             @if ( isset( $event->venue->lat ) AND isset( $event->venue->lng ) )
-                                <a href="https://www.google.com/maps/{!! '@'.$event->venue->lat .','. $event->venue->lng . ',17z' !!}"
+                                <a href="https://www.google.com/maps/search/{{ urlencode($event->venue->title) }}/{!! '@'.$event->venue->lat .','. $event->venue->lng . ',17z' !!}"
                                    target="_blank">
                                     View on location Google Maps
                                 </a>
@@ -36,7 +35,7 @@
                     </p>
                     <p><br/><br/></p>
                     <hr/>
-
+          
                 @empty
                     <p style="padding: 30px 0"><em >No events scheduled.</em></p>
                     <hr>
