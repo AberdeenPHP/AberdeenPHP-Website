cda6ebe634d812f2cdc532f24c4bc1e8bbb79b7d
Andy Gaskell <ag@ssofb.co.uk>
2019-02-13 23:43:51 +0000

    tweaks

--- resources/views/pages/sponsor.blade.php
+++ resources/views/pages/sponsor.blade.php
@@ -8,11 +8,33 @@
             <h1>Sponsor AberdeenPHP</h1>
             <p>If you think you could support the User Group in any way we’d love to hear from you, whether it’s 
             financial support, helping with promoting the meetup or providing materials/resources/training to our attendees.</p>
-            
+            <p>At the start of 2019 we introduced a structure sponsorship program.  Within this, we offer two levels, main sponsor and micro sponsor.
+            A main sponsor contributes £500 p/a and a micro sponsor contributes £100 p/a.  As a sponsor, you'll receive the unbridled enthusiasm and 
+            affection of the web development community, generally good vibes, and a logo below, with a wee blurb.</p>
             <hr/>
             
-            <h2>Sponsors</h2>
-            <a href="http://www.fifthring.com/" target="_blank"><img src="images/fifthring.png"></a> 
+            <h2>Main Sponsors</h2>
+            <p>Your logo could be here.<p>
+
+            <hr/>
+
+            <h2>Venue Sponsor</h2>
+            <a href="http://www.fifthring.com/" target="_blank" title="sponsor"><img src="images/fifthring_logo_400w.png" alt="logo"></a> 
+            <p>Integrated global B2B marketing communications: brand and strategy, creative, digital, inbound marketing, PR & Advertising. Specialists in the energy sector.</p>
+
+            <hr/>
+
+            <h2>Micro Sponsors</h2>
+
+            <a href="http://www.ssofb.co.uk" target="_blank" title="sponsor"><img src="images/ssofb_logo_200w.png" alt="logo"></a> 
+            <p>Open source web development in Aberdeen.</p>
+
+            <a href="http://www.piota.co.uk" target="_blank" title="sponsor"><img src="images/piota_logo_200w.png" alt="logo"></a> 
+            <p>One of the UK’s Leading Communication App Providers.</p>
+
+            <hr/>
+
+            <h2>Other Supporters</h2>
             <a href="https://www.jetbrains.com" target="_blank"><img src="images/jetbrains.png"></a>
             
         </div>
