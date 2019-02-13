604402325b3cd0ba30d79bf4d9662099b4ae119f
Andy Gaskell <ag@ssofb.co.uk>
2019-02-13 23:51:39 +0000

    new sponsors details, tweaks

--- resources/views/pages/sponsor.blade.php
+++ resources/views/pages/sponsor.blade.php
@@ -8,34 +8,57 @@
             <h1>Sponsor AberdeenPHP</h1>
             <p>If you think you could support the User Group in any way we’d love to hear from you, whether it’s 
             financial support, helping with promoting the meetup or providing materials/resources/training to our attendees.</p>
-            <p>At the start of 2019 we introduced a structure sponsorship program.  Within this, we offer two levels, main sponsor and micro sponsor.
+            <p>At the start of 2019 we introduced a structured sponsorship program.  Within this, we offer two levels, main sponsor and micro sponsor.
             A main sponsor contributes £500 p/a and a micro sponsor contributes £100 p/a.  As a sponsor, you'll receive the unbridled enthusiasm and 
             affection of the web development community, generally good vibes, and a logo below, with a wee blurb.</p>
+            <p>Please get in touch if you'd like to sponsor the user group, or have any questions about sponsorship.</p>
             <hr/>
             
             <h2>Main Sponsors</h2>
-            <p>Your logo could be here.<p>
 
+            <div class="row">
+                <div class="col-md-6">
+                    <p style="text-align: center">Your logo could be here.<p>
+                </div>
+                <div class="col-md-6">
+                    <p style="text-align: center">Your logo could be here.<p>
+                </div>
+            </div>
             <hr/>
+
 
             <h2>Venue Sponsor</h2>
-            <a href="http://www.fifthring.com/" target="_blank" title="sponsor"><img src="images/fifthring_logo_400w.png" alt="logo"></a> 
-            <p>Integrated global B2B marketing communications: brand and strategy, creative, digital, inbound marketing, PR & Advertising. Specialists in the energy sector.</p>
-
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="http://www.fifthring.com/" target="_blank" title="Sponsor"><img style="max-width: 100%" src="images/fifthring_logo_400w.png" alt="logo"></a> 
+                    <p style="text-align: center">Integrated global B2B marketing communications: brand and strategy, creative, digital, inbound marketing, PR & Advertising. Specialists in the energy sector.</p>
+                </div>
+                <div class="col-md-6">
+                    <p> <p>
+                </div>
+            </div>
             <hr/>
 
+            
             <h2>Micro Sponsors</h2>
-
-            <a href="http://www.ssofb.co.uk" target="_blank" title="sponsor"><img src="images/ssofb_logo_200w.png" alt="logo"></a> 
-            <p>Open source web development in Aberdeen.</p>
-
-            <a href="http://www.piota.co.uk" target="_blank" title="sponsor"><img src="images/piota_logo_200w.png" alt="logo"></a> 
-            <p>One of the UK’s Leading Communication App Providers.</p>
-
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="http://www.ssofb.co.uk" target="_blank" title="Sponsor"><img style="max-width: 100%" src="images/ssofb_logo_200w.png" alt="logo"></a> 
+                    <p style="text-align: center">Open source web development in Aberdeen.</p>
+                </div>
+                <div class="col-md-4">
+                    <a href="http://www.piota.co.uk" target="_blank" title="Sponsor"><img style="max-width: 100%" src="images/piota_logo_200w.png" alt="logo"></a> 
+                    <p style="text-align: center">One of the UK’s Leading Communication App Providers.</p>
+                </div>
+                <div class="col-md-4">
+                    <p style="text-align: center">Your logo could be here.<p>
+                </div>
+            </div>
             <hr/>
+
 
             <h2>Other Supporters</h2>
-            <a href="https://www.jetbrains.com" target="_blank"><img src="images/jetbrains.png"></a>
+            <a href="https://www.jetbrains.com" target="_blank"><img src="jetbrains.png"></a>
             
         </div>
     </div>
